fe47ad5d959a668176b1c1a399a3850f52a0010a
Evaristo Romero Tirado <romeroevaristo2005@gmail.com>
2026-09-05 16:45:44 -0600

    Ejercicio Jennys Secrets Message

---
+++ CodeWars/Java/JennysSecretsMessage.java
@@ -0,0 +1,20 @@
+public class JennysSecretsMessage {
+    public static void main(String[] args) {
+        /*Instrucción 
+        Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+    Can you help her?
+        */
+    }
+    /*Solución
+    public class Greeter {
+    public static String greet(String name) {
+
+
+        if(name.equals("Johnny")){
+            return "Hello, my love!";
+        }
+        return String.format("Hello, %s!", name);
+    }
+    }
+    */
+}
